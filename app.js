d3d1af8103a2b4bcf4492c5137471e9fd7ec03e3
Arpit Jana <arpitarannya@gmail.com>
2021-12-16 12:59:48 +0530

    Project Navbar Compleated

---
+++ app.js
@@ -0,0 +1,8 @@
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+const socialIcons = document.querySelector(".social-icons");
+
+navToggle.addEventListener("click", function() {
+    links.classList.toggle("show-links");
+    socialIcons.classList.toggle("show-social-icons");
+});
